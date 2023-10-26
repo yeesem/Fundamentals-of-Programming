0d37f443cb4434747a64f93dfc7a0c304ef48c7c
OON YEE SEM <117327996+yeesem@users.noreply.github.com>
2023-10-26 20:31:28 +0800

    Add files via upload

---
+++ L2Q1.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package LAB_2;
+
+import java.util.Random;
+
+public class L2Q1 {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int x = random.nextInt(41) + 10;
+        int y = random.nextInt(41) + 10;
+        int z = random.nextInt(41) + 10;
+
+        System.out.printf("The three numbers: %d, %d, %d\n", x, y, z);
+        System.out.printf("The sum of the numbers: %d\n", x + y + z);
+        System.out.printf("The average: %.2f\n", (x + y + z) / 3.0);
+        
+        for (int i = 0; i < 300; i++) {
+            int check = random.nextInt(6) + 2;
+                System.out.println(i + ". " + check);
+        }
+    }
+}
