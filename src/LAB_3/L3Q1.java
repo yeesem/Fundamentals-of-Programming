bb0eaea6d923c475114202e7790ff20b617d7f51
sem <yeessemoon2002@gmail.com>
2023-10-26 21:13:07 +0800

    FOP

---
+++ src/LAB_3/L3Q1.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package LAB_3;
+
+/**
+ *
+ * @author USER
+ */
+public class L3Q1 {
+    
+}
